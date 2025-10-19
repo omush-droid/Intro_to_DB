29eaf80ed4f16d9220f8e01a9c4756cbb1733a52
omush-droid <omushephraim@gmail.com>
2025-10-19 22:09:55 +0300

    Update task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -1,8 +1,5 @@
--- Script to insert data into the Customers table
+-- Script to insert a single row into the Customers table
 USE alx_book_store;
 
 INSERT INTO Customers (customer_id, customer_name, email, address)
-VALUES
-(1, 'John Doe', 'johndoe@example.com', '123 Main Street, Nairobi'),
-(2, 'Jane Smith', 'janesmith@example.com', '456 Park Avenue, Mombasa'),
-(3, 'Michael Brown', 'michaelbrown@example.com', '789 River Road, Kisumu');
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
