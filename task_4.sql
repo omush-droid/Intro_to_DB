dc91d5b8b800e73c957b483f3aadf1364a1b272e
omush-droid <omushephraim@gmail.com>
2025-10-19 22:01:43 +0300

    Update SQL script to show column details of Books
    Modified the script to retrieve column details for the Books table.

--- task_4.sql
+++ task_4.sql
@@ -1,4 +1,7 @@
 -- Script to print the full description of the table books
 USE alx_book_store;
 
-SHOW CREATE TABLE Books;
+SELECT COLUMN_NAME, COLUMN_TYPE
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+AND TABLE_NAME = 'Books';
