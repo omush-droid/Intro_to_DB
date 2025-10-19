c9de1ee7e05eed73ccbe6bbd8e16ed2a24de822f
omush-droid <omushephraim@gmail.com>
2025-10-19 22:05:28 +0300

    Create script to display Books table structure
    Add SQL script to retrieve column details for Books table

---
+++ task_5.sql
@@ -0,0 +1,7 @@
+-- Script to print the full description of the table books
+USE alx_book_store;
+
+SELECT COLUMN_NAME, COLUMN_TYPE
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+AND TABLE_NAME = 'Books';
