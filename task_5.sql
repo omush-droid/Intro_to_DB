7d8f5d298e011add330affc98f43ccd0630c4efd
omush-droid <omushephraim@gmail.com>
2025-10-19 22:07:18 +0300

    Add data to Customers table
    Inserted sample data into the Customers table.

--- task_5.sql
+++ task_5.sql
@@ -1,7 +1,8 @@
--- Script to print the full description of the table books
+-- Script to insert data into the Customers table
 USE alx_book_store;
 
-SELECT COLUMN_NAME, COLUMN_TYPE
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'alx_book_store'
-AND TABLE_NAME = 'Books';
+INSERT INTO Customers (customer_id, customer_name, email, address)
+VALUES
+(1, 'John Doe', 'johndoe@example.com', '123 Main Street, Nairobi'),
+(2, 'Jane Smith', 'janesmith@example.com', '456 Park Avenue, Mombasa'),
+(3, 'Michael Brown', 'michaelbrown@example.com', '789 River Road, Kisumu');
